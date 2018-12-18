8255f1f42d274ff9f020a13f81318ec4be609625
Mobolaji Abiodun <mabiodun@mdsol.com>
2018-12-18 18:44:11 +0000

    added tests

--- src/main/scala/com/mdsol/checkmeoutbot/Main.scala
+++ src/main/scala/com/mdsol/checkmeoutbot/Main.scala
@@ -1,35 +1,8 @@
 package com.mdsol.checkmeoutbot
 
 import com.mdsol.checkmeoutbot.app.CMOBServer
-import com.mdsol.checkmeoutbot.app.controllers.AuthenticateController
-
-object Main extends CMOBServer with App {
+import com.mdsol.checkmeoutbot.app.services.GithubServices
 
 
-
-  val command = scala.io.StdIn.readLine()
-  val authenticator = new AuthenticateController
-
-  def installCheckMeOutBot() = {
-    println("Installing ...")
-    authenticator.authenticate()
-
-
-  }
-
-  def getAccessToken(): Unit = {
-    println("getting access token ...")
-  }
-
-
-  def subscribeToCheckMeOutBot(command: String) = ???
-
-  command.split(" ")(0) match {
-    case "install" => installCheckMeOutBot()
-    case "subscribe" => subscribeToCheckMeOutBot(command)
-    case "access" => getAccessToken()
-  }
-
-
-
+object Main extends CMOBServer with App with GithubServices {
 }
